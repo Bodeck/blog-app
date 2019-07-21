200aafdc440beac803449c8851b3d101400bec61
Bogus Franczak <bfranczak@gmail.com>
2019-07-21 13:40:29 +0200

    Create post list component

--- client/src/components/features/Posts/Posts.js
+++ client/src/components/features/Posts/Posts.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import PostsList from '../PostsList/PostsList';
 
 class Posts extends React.Component {
-  
+
   componentDidMount() {
-   const { loadPosts } = this.props;
-   loadPosts();
+    const { loadPosts } = this.props;
+    loadPosts();
   }
 
   render() {
     const { posts } = this.props;
-    return(
-        <div>
-          Posts
-          <ul>
-            {posts.map(post => <li key={post.id}>{post.title}</li>)}
-          </ul>
-        </div>
+    return (
+      <div>
+        <PostsList posts={posts} />
+      </div>
     );
   }
 }
