1deee93b225f1b1994f9f57ef72b2b07ec4eb4da
Bogus Franczak <bfranczak@gmail.com>
2019-07-24 20:35:08 +0200

    Add single post endpoint

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -9,3 +9,12 @@ exports.getPosts = async (req, res) => {
     res.status(500).json(err);
   }
 }
+
+exports.getPostById = async (req, res) => {
+  
+  try {
+    res.status(200).json(await Post.find({ id: req.params.id }));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
