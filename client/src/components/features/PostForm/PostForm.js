781ae96a5be14bed5124997c9070e782da63431c
Bodeck <bfranczak@gmail.com>
2019-12-29 21:42:03 +0100

    Implement new post form editor

--- client/src/components/features/PostForm/PostForm.js
+++ client/src/components/features/PostForm/PostForm.js
@@ -5,6 +5,10 @@ import { connect } from 'react-redux';
 import TextField from '../../common/TextField/TextField';
 import SectionTitle from '../../common/SectionTitle/SectionTitle';
 import Button from '../../common/Button/Button';
+import Editor from 'react-medium-editor';
+import 'medium-editor/dist/css/medium-editor.css';
+import 'medium-editor/dist/css/themes/default.css';
+import './PostForm.scss';
 
 class PostForm extends React.Component {
 
@@ -16,22 +20,43 @@ class PostForm extends React.Component {
     }
   }
 
+  handleChange = (e) => {
+    const { post } = this.state;
+    this.setState({ post: { ...post, [e.target.name]: e.target.value } })
+  }
+
+  handleEditor = (text) => {
+    const { post } = this.state;
+    this.setState({ post: { ...post, content: text } });
+  }
+
   render() {
     const { post } = this.state;
+    const { handleChange, handleEditor } = this;
+
     return (
       <div>
         <TextField
           label="Title:"
           value={post.title}
+          onChange={handleChange}
+          name="title"
         />
 
         <TextField
           label="Author:"
           value={post.author}
+          onChange={handleChange}
+          name="author"
         />
-        
-        <SectionTitle>Edit post content</SectionTitle>
 
+        <SectionTitle>Edit post content</SectionTitle>
+        <Editor
+          className="content-editor"
+          text={post.content}
+          options={{ placeholder: false, toolbar: { buttons: ['bold', 'underline', 'italic', 'anchor', 'h2', 'h3', 'quote'] } }}
+          onChange={handleEditor}
+        />
         <Button variant="primary">Add post</Button>
 
       </div>
