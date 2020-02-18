5ba02a80e517ec1c643d30502264ff1e899fa3db
Bodeck <bfranczak@gmail.com>
2020-02-18 08:40:01 +0100

    WIP: create end point to update and delete posts

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -15,4 +15,10 @@ router.route('/posts').post(PostController.addPost);
 // get post by range
 router.route('/posts/range/:startAt/:limit').get(PostController.getPostsByRange);
 
+// delete single post by id
+router.route('/posts/:id').delete(PostController.deletePostById);
+
+// update post
+router.route('/posts/:id').put(PostController.updatePost);
+
 module.exports = router;
