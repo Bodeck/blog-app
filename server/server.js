8d0f31331ad487595063236a318bd7d7792cbf5d
Bogus Franczak <bfranczak@gmail.com>
2021-04-18 11:49:12 +0200

    Fix db connection

--- server/server.js
+++ server/server.js
@@ -1,36 +1,38 @@
-const config = require('./config');
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const loadTestData = require('./testData');
-const helmet = require('helmet');
-const path = require('path');
+const config = require("./config");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const loadTestData = require("./testData");
+const helmet = require("helmet");
+const path = require("path");
 
 const app = express();
 
 // import routes
-const postRoutes = require('./routes/post.routes');
+const postRoutes = require("./routes/post.routes");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/api', postRoutes);
+app.use("/api", postRoutes);
 app.use(helmet());
-app.use(express.static(path.join(__dirname,'/../client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+app.use(express.static(path.join(__dirname, "/../client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
 });
-
 
 // connect to database
-mongoose.connect(config.DB, { useNewUrlParser: true });
-let db = mongoose.connection;
-db.once('open', () => {
-  console.log('Connected to database');
-  loadTestData();
-});
-db.on('error', (err) => console.log(`Error ${err}`));
+mongoose
+  .connect(config.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to database");
+    loadTestData();
+  })
+  .catch((err) => console.log(`Error ${err}`));
 
 app.listen(config.PORT, () => {
-  console.log('Server is running at port:', config.PORT);
+  console.log("Server is running at port:", config.PORT);
 });
